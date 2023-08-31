64681c3cf12dd404efb91ea7abe0078832be6577
hemantRawat <hemantrawat812@gmail.com>
2023-08-31 23:48:34 +0530

    Adding LinkList  Remove Nth Node From End of List

--- LinkList/Remove Nth Node From End of List.java
+++ LinkList/Remove Nth Node From End of List.java
@@ -1,5 +1,30 @@
-package LinkList;
-
-public class Remove Nth Node From End of List {
-    
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy= new ListNode(0,head);
+        ListNode curr = dummy;
+        int count = 0;
+        int k = 0;
+        if(head== null || head.next== null){
+            return null;
+        }
+        
+        while (curr.next != null){
+            curr= curr.next;
+            count++;
+        }
+        curr = dummy;
+        k = count - n; 
+        count = 0; 
+        if(k == 0){
+            head = head.next;
+        }
+        else{
+        while (count != k && curr.next != null){
+            curr = curr.next;
+            count++;
+        }
+        }
+        curr.next = curr.next.next;
+        return head;     
+    }
 }
